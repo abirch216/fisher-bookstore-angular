0ae7cbbeb7d14d3e9b06021254a874a49c39ba98
Alex Birchfield <alexbirchfield@Alexs-MacBook-Pro.local>
2018-02-20 17:33:05 -0500

    first commit

---
+++ src/app/books/mock-books-service.ts
@@ -0,0 +1,6 @@
+import { Book } from './book';
+
+export const BOOKS: Book[] = [
+    { id: 1, title: 'Ready Payer One', author: 'Ermest CLine' },
+    { id: 2, title:'Catch 22', author: 'Joseph Heller' }
+];
